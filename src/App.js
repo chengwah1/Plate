45a60fa60435d29cf4391722b0db0b5f143eaa4b
cheng-wah <chengwah1@hotmail.com>
2019-03-28 18:12:47 +0800

    initial commit added header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/logo-via-logohub.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <header className="header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <form className="search">
+              <input type="text" className="search__field" placeholder="Search over 1,000,000 recipes..."/>
+              <button class="btn search__btn">
+                <svg class="search__icon">
+                    <use href="img/icons.svg#icon-magnifying-glass"></use>
+                </svg>
+                <span>Search</span>
+              </button>
+            </form>
+            <div className="likes">
+              <div class="likes__field">
+                  <svg class="likes__icon">
+                      <use href="img/icons.svg#icon-heart"></use>
+                  </svg>
+              </div>
+              <div class="likes__panel">
+                  <ul class="likes__list">
+                      <li>
+                          <a class="likes__link" href="#23456">
+                              <figure class="likes__fig">
+                                  <img src="img/test-1.jpg" alt="Test" />
+                              </figure>
+                              <div class="likes__data">
+                                  <h4 class="likes__name">Pasta with Tomato ...</h4>
+                                  <p class="likes__author">The Pioneer Woman</p>
+                              </div>
+                          </a>
+                      </li>
+                  </ul>
+              </div>
+            </div>
+          </header>
+        </div>
       </div>
     );
   }

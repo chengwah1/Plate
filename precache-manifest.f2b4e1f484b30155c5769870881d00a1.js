a6d74ff05888abb6547aff6947f86d561cdbba04
cheng-wah <chengwah1@hotmail.com>
2019-04-03 21:33:17 +0800

    Updates

---
+++ precache-manifest.f2b4e1f484b30155c5769870881d00a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17531c5a9b1eac8b5140",
+    "url": "/Plate/static/js/runtime~main.931cceb5.js"
+  },
+  {
+    "revision": "ce0c09007285a6cb0b07",
+    "url": "/Plate/static/js/main.c421c2ce.chunk.js"
+  },
+  {
+    "revision": "a3c6b4633c6b116e4afd",
+    "url": "/Plate/static/js/2.3ff58dd3.chunk.js"
+  },
+  {
+    "revision": "ce0c09007285a6cb0b07",
+    "url": "/Plate/static/css/main.19951b30.chunk.css"
+  },
+  {
+    "revision": "45823fbba76ba345754977e65d5c0b4b",
+    "url": "/Plate/index.html"
+  }
+];
